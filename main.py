5a886d3101da7dd36f05343779af81a9654d8b33
Henrique Gaiardoni <167096110+henriquegsantanna@users.noreply.github.com>
2025-04-06 18:10:48 -0300

    Corrigindo erro no Flask

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template
-from views import *
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/chat")
+def chat():
+    return render_template("chat.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
+    
